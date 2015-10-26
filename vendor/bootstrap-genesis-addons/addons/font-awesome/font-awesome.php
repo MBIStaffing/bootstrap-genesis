ce4c1b5d06ad726fd0bcd93305bfc503dfc3061d
Bryan Willis <bryanwillis1@gmail.com>
2015-10-26 04:14:23 -0500

    / ‘vendor/bootstrap-genesis-addons/addons/font-awesome/font-awesome.php’

--- vendor/bootstrap-genesis-addons/addons/font-awesome/font-awesome.php
+++ vendor/bootstrap-genesis-addons/addons/font-awesome/font-awesome.php
@@ -1,10 +1,11 @@
 <?php
 
-// Font Awesome
+// Font Awesome CSS
 
 function bsga_wp_enqueue_style_font_awesome_css() {
     wp_enqueue_style( 'bsga_', get_stylesheet_directory_uri() . '/fonts/font-awesome.min.css', array('jquery'), $version, true );
 }
 
+add_action( 'wp_enqueue_scripts', 'bsga_wp_enqueue_style_font_awesome_css' );
 
 
